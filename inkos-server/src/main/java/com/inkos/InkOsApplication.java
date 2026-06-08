b4635317daef08c946e0b867c4bb617a42c52078
韩利春 <252279481@qq.com>
2026-06-08 11:38:01 +0800

    feat(inkos-server): scaffold Spring Boot 3.x project with Maven

---
+++ inkos-server/src/main/java/com/inkos/InkOsApplication.java
@@ -0,0 +1,11 @@
+package com.inkos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkOsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InkOsApplication.class, args);
+    }
+}
